8ca2f3709013bc0cd62e24acb131811b8e7395f8
Julian Sanchez <julianwsan@gmail.com>
2026-04-19 23:19:24 -0400

    new test cases for running echelon.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 mod matrix;
-// mod operations;
+mod operations;
 
-use std::io::{self};
 use matrix::Matrix;
-// use operations::{row_ops, echelon, arithmetic};
+use operations::echelon::{row_echelon, reduced_row_echelon};
 
 fn main() {
     let mut m1 = Matrix::new(3, 3, vec![ 1.0, 2.0, 3.0, 2.0, 4.0, 6.0, 1.0, 1.0, 1.0]);
@@ -21,6 +20,22 @@ fn main() {
 
     print_section("Printing Number of Columns in M1");
     println!("{}", m1.get_cols());
+
+    print_section("Performaing Row Echelon Reduction");
+    print_section("BEFORE");
+    m2.print_matrix();
+
+    print_section("AFTER");
+    row_echelon(&mut m2);
+    m2.print_matrix();
+
+    print_section("Performaing Reduced Row Echelon Reduction");
+    print_section("BEFORE");
+    m2.print_matrix();
+
+    print_section("AFTER");
+    reduced_row_echelon(&mut m2);
+    m2.print_matrix();
 
 
 }
