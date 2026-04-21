8aa49e27171e68872abf28c5d562245d6b8886e9
Julian Sanchez <julianwsan@gmail.com>
2026-04-20 21:49:44 -0400

    subtraction function added; essentially copied and pasted the addition function

--- src/operations/arithmetic.rs
+++ src/operations/arithmetic.rs
@@ -19,3 +19,21 @@ pub fn add(m1: &Matrix, m2: &Matrix) -> Result<Matrix, String>{
 
     Ok(Matrix::new(m1.get_rows(), m1.get_cols(), data))
 }
+
+pub fn subtract(m1: &Matrix, m2: &Matrix) -> Result<Matrix, String>{
+    if m1.get_rows() != m2.get_rows() || m1.get_cols() != m2.get_cols(){
+        return Err(format!(
+            "Dimension mismatch: ({}x{}) vs ({}x{})",
+            m1.get_rows(), m1.get_cols(),
+            m2.get_rows(), m2.get_cols()
+        ));
+    }
+
+    let data: Vec<f64> = m1.read_matrix()
+        .iter()
+        .zip(m2.read_matrix().iter())
+        .map(|(a, b)| a - b)    // replaced with '-' for subtraction
+        .collect();
+
+    Ok(Matrix::new(m1.get_rows(), m1.get_cols(), data))
+}
